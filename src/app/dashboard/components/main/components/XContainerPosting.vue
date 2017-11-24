265c9246567c57af4b19099b5f521fc04306dafc
ezioadsr <ezioadsr@gmail.com>
2017-11-24 01:11:09 -0200

    #3 Home

---
+++ src/app/dashboard/components/main/components/XContainerPosting.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="XContainerPosting">
+    </div>
+</template>
+<script>
+  export default {
+    name: 'XContainerPosting'
+  }
+</script>
+<style lang="stylus">
+    .XContainerPosting
+
+</style>
